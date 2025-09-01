ec193dfa3d3a456a4528c41f78319fd30e5fc571
Kickend <kickendsurs@gmail.com>
2025-09-01 17:25:02 -0300

    FIEXD

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require("express")
 const server = express();
 
 server.get("/", (req, res) => {
+  res.send("Esta funcionando, Rota padrao :))))")
+})
 
+server.get("/users", (req, res) => {
+    res.send("Usuarios")
 })
 
 server.listen(process.env.PORT, () => {
